2a2c7258be5bdea818897683282301cb5bf5c37f
Mariya Podosinova <mariya.podosinova@gmail.com>
2021-06-02 09:05:12 +0100

    sdd solution untill 27

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -204,8 +204,14 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(num) {
+  if (num <= 1) return false;
+  if (num % 2 === 0 && num > 2) return false;
+  const s = Math.sqrt(num);
+  for (let i = 3; i <= s; i += 2) {
+    if (num % i === 0) return false;
+  }
+  return true;
 }
 
 /**
